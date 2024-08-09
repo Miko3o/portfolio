565e18eff16e12f1d1180c642a5a45fae199690f
Miko3o <mikomunoz459@gmail.com>
2024-08-08 19:48:32 -0700

    fix: one more unused decloration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 import { ProfileDescription } from "../components/ProfileDescription";
 import { Projects } from "../components/Projects";
